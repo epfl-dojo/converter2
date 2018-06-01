452f576cbca9cfc6f6ebd2e3b37bb0c87093a895
chocom00n <hadi_moussaoui@hotmail.com>
2018-06-01 12:00:49 +0200

    dojo 01.06.2018

--- src/converter2/Fenetre.java
+++ src/converter2/Fenetre.java
@@ -9,59 +9,72 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 /**
  *
  * @author DOJO Team
  */
 public class Fenetre extends JFrame {
-
-    private JPanel buttonsPanel = new JPanel();
-    private JPanel radioPanelCenter = new JPanel();
-    private JPanel inputText = new JPanel();
-    //private JPanel sud = new JPanel();
-    private JTextField Input = new JTextField();
-    private JRadioButton rb1 = new JRadioButton("$2CHF");
-    private JRadioButton rb2 = new JRadioButton("CHF2$");
-    private JLabel result= new JLabel();
-    private JButton convert = new JButton("Convert");
-    private JButton reset = new JButton("Reset");
     
+    private JPanel container = new JPanel();
+    private JPanel panel_SUP = new JPanel();
+    private JPanel panel_INF = new JPanel();
+    private JTextField txt_CHF = new JTextField();
+    private JTextField txt_EURO = new JTextField();
+    private JRadioButton rb_CHF2EURO = new JRadioButton("CHF => EURO");
+    private JRadioButton rb_EURO2CHF = new JRadioButton("EURO => CHF");
+    private JLabel lbl_CHF = new JLabel("CHF ");
+    private JLabel lbl_EURO = new JLabel(" EURO");
+    private JButton btn_Convert = new JButton("Convert");
+    private JButton btn_Reset = new JButton("Reset");
+    private ButtonGroup btngrp = new ButtonGroup();
     
-            
     public Fenetre() {
         this.setTitle("Converter"); // Titre de la fenêtre
-        //this.setSize(550, 250); // Taille de la fenêtre en (x,y)
+        this.setSize(450, 120); // Taille de la fenêtre en (x,y)
         this.setLocationRelativeTo(null); // La fenêtre apparaît au milieu de l'écran
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Action à la fermeture de la fenêtre
         this.setResizable(false); // Possibilité de redimensionner la
         this.setLayout(new BorderLayout());
-        
-        this.buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.X_AXIS));
-        //this.setAlignmentX(Component.CENTER_ALIGNMENT);
-        buttonsPanel.add(convert);
-        buttonsPanel.add(Box.createRigidArea(new Dimension(5,0)));
-        buttonsPanel.add(reset);
-        convert.addActionListener(new Clickconvert());
-        // reset.addActionListener(l);
-        add(buttonsPanel, BorderLayout.SOUTH);
-        
-        radioPanelCenter.setLayout(new BoxLayout(radioPanelCenter, BoxLayout.X_AXIS));
-        radioPanelCenter.add(rb1);
-        radioPanelCenter.add(rb2);
-        radioPanelCenter.add(result);
-        radioPanelCenter.setAlignmentX(Component.CENTER_ALIGNMENT);
-        add(radioPanelCenter, BorderLayout.CENTER);
 
-        inputText.add(Input);
-        Input.setPreferredSize(new Dimension(100,20));
+        this.setContentPane(container);
         
-        add(inputText, BorderLayout.NORTH);
+        btngrp.add(rb_CHF2EURO);
+        btngrp.add(rb_EURO2CHF);
+        rb_CHF2EURO.setSelected(true);
+        txt_CHF.setEditable(true);
+        txt_EURO.setEditable(false);
+       
+        rb_CHF2EURO.addActionListener(new CHFListener());
+        rb_EURO2CHF.addActionListener(new EUROListener());
         
-        //this.setContentPane(container);
-        pack();
+        Border padding = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+        container.setBorder(padding);
+        container.setLayout(new BorderLayout());
+        panel_SUP.setLayout(new BoxLayout(panel_SUP, BoxLayout.X_AXIS));
+        panel_SUP.add(lbl_CHF);
+        panel_SUP.add(txt_CHF);
+        panel_SUP.add(rb_CHF2EURO);
+        panel_SUP.add(rb_EURO2CHF);
+        panel_SUP.add(txt_EURO);
+        panel_SUP.add(lbl_EURO);
+        panel_SUP.setAlignmentX(Component.CENTER_ALIGNMENT);
+        container.add(panel_SUP, BorderLayout.CENTER);
+
+
+
+        container.add(panel_SUP, BorderLayout.NORTH);
+
+     
+        panel_INF.add(btn_Convert);
+        panel_INF.add(Box.createRigidArea(new Dimension(5, 0)));
+        panel_INF.add(btn_Reset);
+        btn_Convert.addActionListener(new Clickconvert());
+        container.add(panel_INF, BorderLayout.SOUTH);
+
         this.setVisible(true);
-               
+
     }
 
     // ***
@@ -71,7 +84,27 @@ public class Fenetre extends JFrame {
         public void actionPerformed(ActionEvent e) {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
-        
+
+    }
     
+    class CHFListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+             txt_CHF.setEditable(true);
+             txt_EURO.setEditable(false);
+        }
+
+    }
+     class EUROListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+            txt_CHF.setEditable(false);
+            txt_EURO.setEditable(true);
+        }
+
     }
 }
